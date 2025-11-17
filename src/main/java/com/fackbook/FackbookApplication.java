38c846173a06934fcc22536c26efe9f59ba3f787
MohamedTarek-MTA <mohameddellg15@gmail.com>
2025-11-17 13:43:50 +0200

    initial commit !

---
+++ src/main/java/com/fackbook/FackbookApplication.java
@@ -0,0 +1,13 @@
+package com.fackbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FackbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FackbookApplication.class, args);
+	}
+
+}
